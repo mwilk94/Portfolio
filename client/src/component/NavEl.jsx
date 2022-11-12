2b3712fb51d0a15351643153d3612cbc44fb0696
Merkisha Wilkinson <merkisha94@gmail.com>
2022-11-12 18:45:04 -0500

    pages and folders created

--- client/src/component/NavEl.jsx
+++ client/src/component/NavEl.jsx
@@ -7,11 +7,13 @@ function NavEl() {
     <>
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <img src='./logo.png' alt='logo' width="100" height="100"/>
+          <Nav className="navlinks">
+            <Nav.Link href="/Home">Home</Nav.Link>
+            <Nav.Link href="/About">About Me</Nav.Link>
+            <Nav.Link href="/Portfolio">Potfolio</Nav.Link>
+            <Nav.Link href="/Contact">Contact</Nav.Link>
+            <Nav.Link href="/Resume">Resume</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
