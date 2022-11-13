7db4edac4bb22670b51f3d4a62239d33d4928f36
Merkisha Wilkinson <merkisha94@gmail.com>
2022-11-12 19:04:44 -0500

    created home begining temp

--- client/src/component/home/Home.jsx
+++ client/src/component/home/Home.jsx
@@ -4,7 +4,19 @@ import "./Home.css"
 export default function Home() {
   return (
     <div className='i'>
-      <div className="i-left">left</div>
+      <div className="i-left">
+        <div className="i-left-wrapper">
+          <h2 className='i-intro'>Hello, I'm </h2>
+          <h1 className='i-name'>Merkisha Wilkinson</h1>
+          <div className="i-title">
+            <div className="i-title-wrapper">
+              <div className="i-title-item">Web Developer</div>
+              <div className="i-title-item">Web Developer</div>
+              <div className="i-title-item">Web Developer</div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="i-right">right</div>
     </div>
   )
